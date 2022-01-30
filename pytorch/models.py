9f363b31cbe2f21770cf9cd6b4039152dfa83fcf
sansba <52666718+sansba@users.noreply.github.com>
2022-01-30 12:46:59 +0300

    Update models.py

--- pytorch/models.py
+++ pytorch/models.py
@@ -1,15 +1,20 @@
 import torch.nn as nn
-
-from model_blocks import *
 from torchsummary import summary
 
+from model_blocks import *
+import functional
 
 
 
 #BASIC UNET MODEL
 class UNet(nn.Module):
-    def __init__(self, in_ch, n_classes):
-        super().__init__()
+    def __init__(self, in_ch: int, n_classes: int):
+        """Basic UNet Model. \n
+            Args:
+                - in_ch (int): input channel of input image.
+                - n_classes (int): number of classes. 
+        """
+        super(UNet, self).__init__()
         self.in_ch = in_ch
         self.n_classes = n_classes
         
@@ -43,16 +48,18 @@ class UNet(nn.Module):
 
         return x
 
-    def summary(self, input_size):
-        print(summary(UNet(self.in_ch, self.n_classes), input_size))
-
 
 
 
 #INCEPTION UNET MODEL
-class IncUNet(nn.Module):
-    def __init__(self, in_ch, n_classes):
-        super().__init__()
+class InceptionUNet(nn.Module):
+    def __init__(self, in_ch: int, n_classes: int):
+        """Inception UNet Model. \n
+            Args:
+                - in_ch (int): input channel of input image.
+                - n_classes (int): number of classes. 
+        """
+        super(InceptionUNet, self).__init__()
         self.in_ch = in_ch
         self.n_classes = n_classes
 
@@ -85,18 +92,20 @@ class IncUNet(nn.Module):
 
         return x
 
-    def summary(self, input_size):
-        print(summary(IncUNet(self.in_ch, self.n_classes), input_size))
-
-
 
 
 #ARROW UNET MODEL
 class ArrowUNet(nn.Module):
-    def __init__(self, in_ch, n_classes, channel=56):
-        super().__init__()
+    def __init__(self, in_ch: int, n_classes: int):
+        """Arrow UNet Model. \n
+            Args:
+                - in_ch (int): input channel of input image.
+                - n_classes (int): number of classes. 
+        """
+        super(ArrowUNet, self).__init__()
         self.in_ch = in_ch
         self.n_classes = n_classes
+        channel = 56
 
         self.inconv = ArrInConv(in_ch, channel)
 
@@ -127,5 +136,122 @@ class ArrowUNet(nn.Module):
 
         return x
 
+
+
+
+#SEGMENTATIN MODELS
+class SegmentationModels:
+    def __init__(self, model_name, in_ch, n_classes):
+        """General class for segmentation models. \n
+            Args:
+                - model_name (str): model selection ('unet', 'incunet', 'arrowunet').
+                - in_ch (int): input channel of input image.
+                - n_classes (int): number of classes.
+        """
+        self.models = {"unet":UNet, "inception":InceptionUNet, "arrow":ArrowUNet}
+        self.model_name = model_name
+
+        #Not In the List Error
+        if not self.model_name.lower() in list(self.models.keys()):
+            raise ValueError(f"There is no such a model called {self.model_name}. It must be 'unet', 'inception' or 'arrow'.")
+
+        self.in_ch = in_ch
+        self.n_classes = n_classes
+        self.model = self.models[model_name.lower()](self.in_ch, self.n_classes)
+
+
+        self.train_loader = None
+        self.val_loader = None
+        self.test_loader = None
+
+        self.optimizer = None
+        self.criterions = None
+        self.metrics = None
+        self.callbacks = None
+
+
+    #Fit
+    def fit(self, train_loader, val_loader):
+        """Fits datasets to model. \n
+           Args:
+                - train_loader (DataLoader): training dataset loader.
+                - val_loader (DataLoader): validation dataset loader.
+        """
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+
+
+    #Compile
+    def compile(self, epochs, optimizer, criterions, metrics, callbacks):
+        """Trains model. \n
+            Args:
+                - epochs (int): number of epochs.
+                - batch_size (int): size of each data iter.
+                - optimizer : model optimizer.
+                - criterion : loss function.
+            Examples:
+                >>> model = Model()
+                >>> optimizer = torch.nn.optim.SGD(model.parameters(), lr=0.001)
+                >>> criterion = torch.nn.CrossEntropyLoss()
+                >>> model.fit(train_loader, val_loader)
+                >>> model.compile(20, 32, optimizer, criterion)
+            """
+        if not hasattr(metrics, "__len__"):
+            metrics = [metrics]
+
+        if not hasattr(callbacks, "__len__"):
+            callbacks = [callbacks]
+
+        self.optimizer = optimizer
+        self.criterions = criterions
+        self.metrics = metrics
+        self.callbacks = callbacks
+
+        self.train_accum = functional.Accumulator(self.criterions, self.metrics)
+        self.val_accum = functional.Accumulator(self.criterions, self.metrics)
+
+        functional.train_net(self.train_loader, self.val_loader, self.train_accum, self.val_accum,  self.model, self.optimizer, self.criterions, self.metrics, self.callbacks, epochs, self.print)
+        
+
+    #Predict
+    def predict(self, test_loader, path):
+        """Predicts test data. \n
+            Args:
+                - test_loader (DataLoader): test dataset loader.
+                - path (str): path where predictions will be saved.
+        """
+        self.test_loader = test_loader
+        self.test_accum = functional.Accumulator(self.criterions)
+        functional.predict_test(self.test_loader, self.test_accum, self.model, self.criterions, self.metrics, path)
+    
+
+    #Print
+    def print(self):
+        message_train = "TRAINING: "
+        message_val = "VALIDATION: "
+        for i, name in enumerate(self.train_accum.criterion_names):
+            message_train += f"{name}: " + f"{self.train_accum.criterion_scores[i].float()}\t"
+            message_val += f"{name}: " + f"{self.val_accum.criterion_scores[i].float()}\t"
+
+        print(message_train, message_val, sep="\n")
+
+
+    #Param Upload
+    def param_upload(self, path):
+        """Uploads the parameters of a model. \n
+            Args:
+                - path (str): path to a pt file.
+        """
+        self.model.state_dict(torch.load(path))
+
+
+    #Summary
     def summary(self, input_size):
-        print(summary(ArrowUNet(self.in_ch, self.n_classes), input_size))
+        """Prints model summary.\n
+            Args:
+                - inputs_size (tuple): (H, W) \n
+                where H: height,  W: width
+        """
+        input_size = (self.in_ch, ) + input_size
+        summary(self.model, input_size)
+        print("Model Name: ", self.model.__class__.__name__)
