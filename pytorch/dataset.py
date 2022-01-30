6589bcfc82de1b630ffa9ad009b7af0ae2fbc527
sansba <52666718+sansba@users.noreply.github.com>
2022-01-30 12:45:18 +0300

    Update dataset.py

--- pytorch/dataset.py
+++ pytorch/dataset.py
@@ -2,113 +2,47 @@ import torch
 from torch.utils import data
 from sklearn.model_selection import train_test_split
 
-import nibabel as nib
-import numpy as np
-
 import cv2
-
-import tarfile
-import patoolib as ptl
-
 import glob
 import os
 
 
 
-#Extract Main File > OK
-def extract_tar(from_path, to_path):
-    #Create path if it does not exist
-    if not os.path.isdir(to_path):
-        os.makedirs(to_path, exist_ok=True)
-
-    #TAR file extracting process
-    print("Extracting Data...")
-    tar = tarfile.open(from_path)
-    tar.extractall(to_path)
-    tar.close()
-
-
-
-#Extract GZ Files > OK
-def extract_gz(path):
-    #Path finding process
-    for path1 in glob.glob(path + "/*"):
-        for path2_f, path2_s in zip(glob.glob(path1 + "/*_flair*"), glob.glob(path1 + "/*_seg*")):
-            #Out direction of flair and seg
-            out_dir_f = path2_f.split(".")[0]
-            out_dir_s = path2_s.split(".")[0]
-
-            #Create out directions
-            os.mkdir(out_dir_f)
-            os.mkdir(out_dir_s)
-
-            #Extract corresponding data to output path
-            ptl.extract_archive(path2_f, outdir=out_dir_f)
-            ptl.extract_archive(path2_s, outdir=out_dir_s)
-
-
-
-
-#Create Image > OK
-def create_nii_slices(path, to):
-    #Find nii files of flair and seg
-    for path1 in glob.glob(path + "/*"):
-        for path2_f, path2_s in zip(glob.glob(path1 + "/*_flair"), glob.glob(path1 + "/*_seg")):
-            for path3_f, path3_s in zip(glob.glob(path2_f + "/*_flair*"), glob.glob(path2_s + "/*_seg*")):
-                #Path for output
-                patient_num = path1.split("/")[-1]
-                flair_name = patient_num + "_flair"
-                seg_name = patient_num + "_seg"
-
-                #Create output paths if it does not exist
-                if not os.path.exists(os.path.join(to, patient_num, flair_name)):
-                    os.makedirs(os.path.join(to, patient_num, flair_name))
-                    os.makedirs(os.path.join(to, patient_num, seg_name))
-                
-                #Read nii files
-                flair = nib.load(path3_f).get_fdata()
-                seg = nib.load(path3_s).get_fdata()
-
-                #Create image files
-                for i in range(flair.shape[2]):
-                    if (seg[:, :, i] == 1).any() or (seg[:, :, i] == 2).any() or (seg[:, :, i] == 4).any():
-                        
-                        #Turn images into integer
-                        flair_img = flair[:, :, i]
-                        flair_img = np.uint8(flair_img / flair_img.max() * 255)
-                        seg_img = seg[:, :, i]
-                        seg_img = np.uint8(seg_img)
-
-                        #Flair and seg paths
-                        flair_path = os.path.join(to, patient_num, flair_name, str(i) + ".png")
-                        seg_path = os.path.join(to, patient_num, seg_name, str(i) + ".png")
-
-                        #Create images
-                        cv2.imwrite(flair_path, flair_img)
-                        cv2.imwrite(seg_path, seg_img)
-
-
-
-
-#Read images path as a list > OK
-def read_data_paths(main_path):
-    flairs_path = os.path.join(main_path, "data", "*", "data", "*_flair", "*.png")
-    segs_path = os.path.join(main_path, "data", "*", "data", "*_seg", "*.png")
+#READ DATA PATHS
+def read_data_paths(main_path, n_data):
+    """Create a list that store input and mask images [input_1, mask_1] ... [input_n, mask_n]. \n
+        Args:
+            - main_path (str): main path where data is stored.
+            - n_data (int): number of data. \n
+                0: all data
+        """
+    flairs_path = os.path.join(main_path, "*", "*_flair", "*.png")
+    segs_path = os.path.join(main_path, "*", "*_seg", "*.png")
 
     data_list = []
     for flair_path, seg_path in zip(glob.glob(flairs_path), glob.glob(segs_path)):
         data_list.append([flair_path, seg_path])
+        if len(data_list) == n_data:
+            break
     return data_list
 
 
 
-#Splitting data into three parts
-def train_val_test_dataset(dataset, test_val_split=0.15):
+#TRAIN VAL TEST SPLIT
+def train_val_test_split(dataset, test_val_split=0.15):
+    """Split dataset into train, validation and test data. \n
+        Args:
+            - dataset (DataLoader): dataset to be splitted.
+            - test_val_split (float): splitting ratio between 0 and 1.
+                Default: 0.15
+            """
+    if test_val_split <= 0 or test_val_split >= 1:
+        raise ValueError("Given test_val_split value ({}) causes ambiguity. It must be between 0 and 1.".format(test_val_split))
+
     train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=test_val_split)
     train_idx, test_idx = train_test_split(train_idx, test_size=test_val_split)
     datasets = {}
     
-
     datasets['train'] = data.Subset(dataset, train_idx)
     datasets['val'] = data.Subset(dataset, val_idx)
     datasets["test"] = data.Subset(dataset, test_idx)
@@ -117,10 +51,18 @@ def train_val_test_dataset(dataset, test_val_split=0.15):
 
 
 
-#Create Dataset
+#BRATS DATASET
 class BratsDataset(data.Dataset):
-    def __init__(self, main_path, transforms=None):
-        self.data_list = read_data_paths(main_path)
+    def __init__(self, main_path, n_data, transforms=None):
+        """Creates brats dataset for segmentation tasks. \n
+            Args:
+                - main_path (str): path to the datas.
+                - n_data (int): number of data.\n
+                    0: all data
+                - transforms (Transforms): transforms to be applied to the input and mask images.
+                    Default: None
+        """
+        self.data_list = read_data_paths(main_path, n_data)
         self.transforms = transforms
 
 
@@ -128,16 +70,68 @@ class BratsDataset(data.Dataset):
         return len(self.data_list)
 
 
-    def __getitem__(self, idx):
-        flair = cv2.imread(self.data_list[idx][0], 0)
-        seg = cv2.imread(self.data_list[idx][1], 0)
-        seg[seg == 4] = 3
+    def __getitem__(self, index):
+        img = cv2.imread(self.data_list[index][0], 0)
+        mask = cv2.imread(self.data_list[index][1], 0)
+        mask[mask == 4] = 3
 
-        flair = torch.from_numpy(flair).unsqueeze(0).float()
-        seg = torch.from_numpy(seg).int().type("torch.LongTensor")
+        img = torch.from_numpy(img).unsqueeze(0).float()
+        mask = torch.from_numpy(mask).int().type("torch.LongTensor")
 
         if self.transforms is not None:
-            flair = self.transforms(flair)
-            seg = self.transforms(seg)
+            img = self.transforms(img)
+            mask = self.transforms(mask)
 
-        return flair, seg
+        return img, mask
+
+
+
+#GENERAL DATASET
+class GeneralDataSet(data.Dataset):
+    def __init__(self, data_list, img_ch=1, mask_ch=1, transforms=None):
+        """Creates dataset for segmentation tasks. \n
+            Args:
+                - data_list (list): path to the datas. data_list format should be [[img1, mask1], [img2, mask2], .... [imgn, maskn]]
+                - img_ch (int): channels of input image     1: gray scale   3: rgb
+                    Default: 1
+                - mask_ch (int): channels of mask image     1: gray scale   3: rgb
+                    Default: 1
+                - transforms (Transforms): transforms to be applied to the input and mask images.
+                    Default: None
+        """
+        self.data_list = data_list
+        self.transforms = transforms
+        self.img_ch = img_ch
+        self.mask_ch = mask_ch
+        
+
+    def __len__(self):
+        return len(self.data_list)
+
+    
+    def forward(self, index):
+        if(self.img_ch == 1):
+            img = cv2.imread(self.data_list[index][0], cv2.IMREAD_GRAYSCALE)
+            img = torch.from_numpy(img).float().unsqueeze(0) / img.max()
+
+        elif(self.img_ch == 3):
+            img = cv2.imread(self.data_list[index][0])
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = torch.from_numpy(img).float() / img.max()
+            img = torch.permute(img, (2, 0, 1))
+
+        if(self.mask_ch == 1):
+            mask = cv2.imread(self.data_list[index][1], cv2.IMREAD_GRAYSCALE)
+            mask = torch.from_numpy(mask).int().unsqueeze(0)
+
+        elif(self.mask_ch == 3):
+            mask = cv2.imread(self.data_list[index][1])
+            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+            mask = torch.from_numpy(mask).int()
+            mask = torch.permute(mask, (2, 0, 1))
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+            mask = self.transforms(mask)
+        
+        return img, mask
